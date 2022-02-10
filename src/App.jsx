770834e3e0786bf5d46aff283667f8e9594d4f94
Elamir <engineeralamiir@gmail.com>
2022-02-10 17:15:20 +0200

    Single Page Primary Design Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/system";
 import React from "react";
 import ProductsPage from "./mainComponents/ProductsPage";
+import SingleProductPage from "./mainComponents/SingleProductPage";
+import HeaderProductsPage from "./subComponents/HeaderProductsPage";
 
 const App = () => {
   return (
-    <Box>
-      <ProductsPage />
+    <Box sx={{ padding: "0px 10px 10px 101px", fontFamily: "Raleway" }}>
+      <HeaderProductsPage />
+      {/* <ProductsPage /> */}
+      <SingleProductPage />
     </Box>
   );
 };
--- src/mainComponents/ProductsPage.jsx
+++ src/mainComponents/ProductsPage.jsx
@@ -7,19 +7,16 @@ import CartProductPage from "../subComponents/CartProductPage";
 
 const ProductsPage = () => {
   return (
-    <Box sx={{ padding: "0px 10px 10px 101px", fontFamily: "Raleway" }}>
+    <Box>
       {/* Header */}
-      <HeaderProductsPage />
-      <Box>
-        <p style={{ fontSize: "42px", fontFamily: "Raleway" }}>Category Name</p>
-        <Box sx={{ display: "grid", gridTemplateColumns: "repeat(3,1fr)" }}>
-          <CartProductPage />
-          <CartProductPage />
-          <CartProductPage />
-          <CartProductPage />
-          <CartProductPage />
-          <CartProductPage />
-        </Box>
+      <p style={{ fontSize: "42px", fontFamily: "Raleway" }}>Category Name</p>
+      <Box sx={{ display: "grid", gridTemplateColumns: "repeat(3,1fr)" }}>
+        <CartProductPage />
+        <CartProductPage />
+        <CartProductPage />
+        <CartProductPage />
+        <CartProductPage />
+        <CartProductPage />
       </Box>
     </Box>
   );
